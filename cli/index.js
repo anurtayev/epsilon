f47b12306284e68d9f08affeb3aad2f1219d1433
Adil Nurtayev <anurtayev@torstar.ca>
2021-12-10 00:44:08 -0500

    implemented appsync with cloudformation

--- cli/index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv-safe").config();
-
-const { program } = require("commander");
-
-program.addCommand(require("./walkers/walkS3"));
-program.addCommand(require("./walkers/walkDisk"));
-
-program.parseAsync(process.argv);
