6bb3525535a7644ac9605693bbd850a35f5c087d
Adil Nurtayev <adilnurtayev@Adils-MacBook-Air.local>
2021-12-25 09:46:00 -0500

    created event bridge

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,24 +12,36 @@ tsc || exit 1
 docker build -t epsilon-deploy .
 docker cp $(docker create --rm epsilon-deploy):/app/node_modules build/node_modules
 mkdir deploy
-zip deploy/deployment-package.zip build/* -r -q
+zip deploy/$PROJECT-$NODE_ENV.zip build/* -r -q
 
 aws s3 sync cloudformation s3://$S3_DEPLOYMENT_BUCKET/cloudformation || exit 1
-aws s3 sync deploy s3://$S3_DEPLOYMENT_BUCKET || exit 1
+#aws s3 sync deploy s3://$S3_DEPLOYMENT_BUCKET || exit 1
 
 aws cloudformation validate-template \
   --template-url https://$S3_DEPLOYMENT_BUCKET.s3.amazonaws.com/cloudformation/appsync.yml \
   || exit 1
 
-#aws cloudformation create-stack --stack-name aspan-epsilon-appsync --template-url https://%S3_DEPLOYMENT_BUCKET%.s3.amazonaws.com/cloudformation/appsync.yml --parameters ParameterKey=ApiName,ParameterValue=AspanEpsilonDev
-
-aws cloudformation update-stack \
-  --stack-name aspan-epsilon-appsync \
-  --template-url https://$S3_DEPLOYMENT_BUCKET.s3.amazonaws.com/cloudformation/appsync.yml --parameters ParameterKey=ApiName,ParameterValue=AspanEpsilonDev \
-  --parameters ParameterKey=DeploymentPackageArchive,ParameterValue=deployment-package.zip \
-  --capabilities CAPABILITY_NAMED_IAM \
+if aws cloudformation describe-stacks --stack-name $PROJECT-$NODE_ENV
+then
+  aws cloudformation update-stack \
+    --stack-name $PROJECT-$NODE_ENV \
+    --template-url https://$S3_DEPLOYMENT_BUCKET.s3.amazonaws.com/cloudformation/appsync.yml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters \
+      ParameterKey=ProjectName,ParameterValue=$PROJECT \
+      ParameterKey=Environment,ParameterValue=$NODE_ENV \
+      ParameterKey=DeploymentBucketName,ParameterValue=$S3_DEPLOYMENT_BUCKET \
   || exit 1
-
-aws cloudformation describe-stacks --stack-name aspan-epsilon-appsync
+else
+  aws cloudformation create-stack \
+    --stack-name $PROJECT-$NODE_ENV \
+    --template-url https://$S3_DEPLOYMENT_BUCKET.s3.amazonaws.com/cloudformation/appsync.yml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameters \
+      ParameterKey=ProjectName,ParameterValue=$PROJECT \
+      ParameterKey=Environment,ParameterValue=$NODE_ENV \
+      ParameterKey=DeploymentBucketName,ParameterValue=$S3_DEPLOYMENT_BUCKET \
+  || exit 1
+fi
 
     
