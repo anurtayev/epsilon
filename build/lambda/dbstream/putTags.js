5a4cbe5ac4e21ac2db92b066b00a51195c6ed28c
Adil Nurtayev <anurtayev@torstar.ca>
2022-02-21 17:53:54 -0500

    removed build files

--- build/lambda/dbstream/putTags.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const awsClients_1 = require("../../lib/awsClients");
-exports.default = (tags = []) => tags.map((tag) => awsClients_1.documentClient
-    .put({ Item: { tag }, TableName: process.env.TAGS_TABLE })
-    .promise());
-//# sourceMappingURL=putTags.js.map
--- build/lib/graphqlTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SortOrder = void 0;
-var SortOrder;
-(function (SortOrder) {
-    SortOrder["Asc"] = "ASC";
-    SortOrder["Desc"] = "DESC";
-})(SortOrder = exports.SortOrder || (exports.SortOrder = {}));
-//# sourceMappingURL=graphqlTypes.js.map
