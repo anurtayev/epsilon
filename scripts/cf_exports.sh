1517e92dec79c8592fd1deddb902bc53b8330539
Adil Nurtayev <anurtayev@torstar.ca>
2022-03-19 23:40:09 -0400

    fixed schema

--- scripts/cf_exports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. scripts/env.sh
-
-export INFRASTRUCTURE_BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $ASPAN_ENV_NAME-pipeline --query 'Stacks[0].Outputs[?OutputKey==`InfrastructureBucketName`].OutputValue' --output text)
-# export MEDIA_BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name $ASPAN_ENV_NAME-app --query 'Stacks[0].Outputs[?OutputKey==`S3BucketName`].OutputValue' --output text)
