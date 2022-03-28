cb97f62b23e2b789546e43c8d5c67ae609742361
Adil Nurtayev <anurtayev@torstar.ca>
2022-03-28 09:29:23 -0400

    removed schema.graphql

--- scripts/build_template.sh
+++ scripts/build_template.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cat schema.graphql | sed 's/^/        /' > schema-tabbed.graphql
+cat ./node_modules/@aspan/sigma/lib/schema.graphql | sed 's/^/        /' > schema-tabbed.graphql
 cat template-src.yaml | sed '/Definition: |/r ./schema-tabbed.graphql' > template.yaml
 rm schema-tabbed.graphql
