ba94343aa0d5e1f592c48170571bcd71eff3f76b
Adil Nurtayev <anurtayev@torstar.ca>
2022-05-15 19:48:10 -0400

    changed git branch env var

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,7 +5,7 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-export GIT_BRANCH=$(git status|head -n 1|cut -d ' ' -f3)
+# export GIT_BRANCH=$(git status|head -n 1|cut -d ' ' -f3)
 export ASPAN_AWS_ACCOUNT=$(aws sts get-caller-identity --query 'Account' --output text)
 export ASPAN_ENV_NAME=$PROJECT-$GIT_BRANCH-$ASPAN_AWS_ACCOUNT-$AWS_DEFAULT_REGION
 export ASPAN_APP_STACK_NAME=$ASPAN_ENV_NAME-app
