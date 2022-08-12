f830d89c9cd75a4d8828cf93dfc740a48bad4d70
Adil Nurtayev <anurtayev@torstar.ca>
2022-08-11 23:03:29 -0400

    fix(auth): fixed cognito params

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -1,39 +1,54 @@
 const { CognitoJwtVerifier } = require("aws-jwt-verify");
 
+// const CUSTOM_CLAIM_KEY = "application_name";
+// const CUSTOM_CLAIM_VALUE = "Aspan";
+
+const extractCognitoParameters = (authorizationHeader) => {
+  const { cognitoUserPoolId, cognitoUserPoolClientId } = JSON.parse(
+    Buffer.from(token.split(".")[1], "base64").toString("utf8")
+  );
+
+  return { cognitoUserPoolId, cognitoUserPoolClientId };
+};
+
 exports.handler = async function (event, context) {
   const request = event.Records[0].cf.request;
   const headers = request.headers;
 
-  const cognitoUserPoolId = headers["x-cognito-user-pool-id"];
-  const cognitoUserPoolClientId = headers["x-cognito-user-pool-client-id"];
-  const cognitoToken = headers["x-cognito-token"];
-
-  const verifier = CognitoJwtVerifier.create({
-    userPoolId: cognitoUserPoolId,
-    tokenUse: "access",
-    clientId: cognitoUserPoolClientId,
-  });
-
   try {
+    const { cognitoUserPoolId, cognitoUserPoolClientId } =
+      extractCognitoParameters(headers["authorization"]);
+
+    const verifier = CognitoJwtVerifier.create({
+      userPoolId: cognitoUserPoolId,
+      tokenUse: "id",
+      clientId: cognitoUserPoolClientId,
+    });
+
     const payload = await verifier.verify(cognitoToken);
-    console.log("Token is valid. Payload:", payload);
+
+    // TODO: implement custom claim challenge
+    // if (payload[CUSTOM_CLAIM_KEY] !== CUSTOM_CLAIM_VALUE) {
+    //   throw Error("<application_name> claim is not valid");
+    // }
+
     return request;
   } catch (e) {
-    console.log("Token not valid!", e);
+    console.log("invalid token", e);
 
     return {
-      body: "content",
+      body: "Forbidden",
       bodyEncoding: "text",
       headers: {
-        "x-custom-header": [
+        "x-aspan-header": [
           {
-            key: "x-custom-header",
-            value: "custom-value",
+            key: "x-aspan-header",
+            value: CUSTOM_CLAIM_VALUE,
           },
         ],
       },
-      status: "200",
-      statusDescription: "OK",
+      status: "403",
+      statusDescription: "Forbidden",
     };
   }
 };
