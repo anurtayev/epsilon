3654d264cdbde2d4d8444c7c5fb66d97c1dbc2ea
Adil Nurtayev <anurtayev@torstar.ca>
2022-08-13 15:22:28 -0400

    feat(auth): added cognito trigger

---
+++ auth/verifyToken-src.js
@@ -0,0 +1,75 @@
+const { CognitoJwtVerifier } = require("aws-jwt-verify");
+
+const AUTHORIZATION_HEADER_LOWERCASE = "authorization";
+const CUSTOM_CLAIM_KEY = "aspan_custom_claim";
+const CUSTOM_CLAIM_VALUE = "";
+
+const extractCognitoParameters = (token) => {
+  console.log("==>  token", token);
+  const decodedToken = Buffer.from(token.split(".")[1], "base64").toString(
+    "utf8"
+  );
+  console.log("==> decoded token", decodedToken);
+
+  const { iss, aud: cognitoUserPoolClientId } = JSON.parse(decodedToken);
+  const cognitoUserPoolId = iss.split("/").slice(-1)[0];
+
+  return { cognitoUserPoolId, cognitoUserPoolClientId };
+};
+
+exports.handler = async function (event, context) {
+  console.log("incoming event", JSON.stringify(event, null, 2));
+  const request = event.Records[0].cf.request;
+  const headers = request.headers;
+  const authHeaderArray =
+    headers[
+      Reflect.ownKeys(headers).find(
+        (header) => header.toLowerCase() === AUTHORIZATION_HEADER_LOWERCASE
+      )
+    ];
+  const authHeader = authHeaderArray.find(
+    ({ key }) => key.toLowerCase() === AUTHORIZATION_HEADER_LOWERCASE
+  );
+
+  try {
+    const cognitoToken = authHeader.value.split("Bearer ")[1];
+
+    const { cognitoUserPoolId, cognitoUserPoolClientId } =
+      extractCognitoParameters(cognitoToken);
+    console.log("==> cognitoUserPoolId", cognitoUserPoolId);
+    console.log("==> cognitoUserPoolClientId", cognitoUserPoolClientId);
+
+    const verifier = CognitoJwtVerifier.create({
+      userPoolId: cognitoUserPoolId,
+      tokenUse: "id",
+      clientId: cognitoUserPoolClientId,
+    });
+
+    const payload = await verifier.verify(cognitoToken);
+
+    console.log("==> payload", payload);
+
+    if (payload[CUSTOM_CLAIM_KEY] !== CUSTOM_CLAIM_VALUE) {
+      throw Error("<application_name> claim is not valid");
+    }
+
+    return request;
+  } catch (e) {
+    console.log("invalid token", e.message, typeof e);
+
+    return {
+      body: "Forbidden",
+      bodyEncoding: "text",
+      headers: {
+        "x-aspan-header": [
+          {
+            key: "x-aspan-header",
+            value: CUSTOM_CLAIM_VALUE,
+          },
+        ],
+      },
+      status: "403",
+      statusDescription: "Forbidden",
+    };
+  }
+};
--- scripts/build_template.sh
+++ scripts/build_template.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+UUID=$(node -e "const {v4} = require('uuid'); console.log(v4())")
+
+cat auth/verifyToken-src.js | sed 's/CUSTOM_CLAIM_VALUE = "";/CUSTOM_CLAIM_VALUE = "'$UUID'";/' > auth/verifyToken.js
+
 cat ./node_modules/@aspan/sigma/lib/schema.graphql | sed 's/^/        /' > schema-tabbed.graphql
-cat infrastructure/template-src.yaml | sed '/Definition: |/r ./schema-tabbed.graphql' > infrastructure/template.yaml
+cat infrastructure/template-src.yaml | sed '/Definition: |/r ./schema-tabbed.graphql' | sed 's/aspan_custom_claim: ""/aspan_custom_claim: "'$UUID'"/' > infrastructure/template.yaml
 rm schema-tabbed.graphql
