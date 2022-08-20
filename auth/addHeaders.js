61dd0c498956d9ab6708a4f4069a82b1dbbae0b9
Adil Nurtayev <anurtayev@torstar.ca>
2022-08-20 10:40:11 -0400

    fix(auth): fixed headers

--- auth/addHeaders.js
+++ auth/addHeaders.js
@@ -2,7 +2,33 @@ exports.handler = async function (event, context) {
   console.log("incoming event", JSON.stringify(event, null, 2));
   const response = event.Records[0].cf.response;
 
-  response.headers["Allowed-shit"] = { value: "sdfsdf" };
+  response.headers["access-control-allow-origin"] = [
+    {
+      key: "Access-Control-Allow-Origin",
+      value: "*",
+    },
+  ];
+
+  response.headers["access-control-max-age"] = [
+    {
+      key: "Access-Control-Max-Age",
+      value: "0",
+    },
+  ];
+
+  response.headers["access-control-expose-headers"] = [
+    {
+      key: "Access-Control-Expose-Headers",
+      value: "*",
+    },
+  ];
+
+  response.headers["access-control-allow-methods"] = [
+    {
+      key: "Access-Control-Allow-Methods",
+      value: "*",
+    },
+  ];
 
   return response;
 };
